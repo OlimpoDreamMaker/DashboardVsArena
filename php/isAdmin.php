5c6c6b5777f3027abf49385e30e5fdcec910866f
Hoyos Juan <hoyosjuanmanuel1@gmail.com>
2020-05-29 14:59:24 -0300

    Function IsAdmin

---
+++ php/isAdmin.php
@@ -0,0 +1,14 @@
+<?php
+
+  function($conexion,$id){
+    $consulta = "SELECT * FROM usuarios WHERE idUsuario='$id'";
+    $resultado = mysqli_query($conexion, $consulta);
+    $user = mysqli_fetch_assoc($resultado);
+    if($user['isAdmin'] ==  1){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+?>
