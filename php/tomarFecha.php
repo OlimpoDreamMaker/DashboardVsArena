842490650ce1f76ff375d801ec799bc7914a10ca
Hoyos Juan <hoyosjuanmanuel1@gmail.com>
2020-05-29 21:59:02 -0300

    Function Tomando Fecha

---
+++ php/tomarFecha.php
@@ -0,0 +1,18 @@
+<?php
+
+  function primeraFecha($fecha){
+    $indiceGuion = strpos($fecha, "-");
+    $longitudFecha = strlen($fecha);
+    $fechaNew = trim(substr($fecha, 0, $indiceGuion-1));
+    $fechaNew = trim(str_replace("/","-",$fechaNew));
+    return $fechaNew;
+  }
+  function segundaFecha($fecha){
+    $indiceGuion = strpos($fecha, "-");
+    $longitudFecha = strlen($fecha);
+    $fechaNew = trim(substr($fecha, $indiceGuion + 1, $longitudFecha));
+    $fechaNew = trim(str_replace("/","-",$fechaNew));
+    return $fechaNew;
+  }
+
+?>
